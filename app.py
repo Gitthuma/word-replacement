af34a3b3c76a5bef43ea6123b9220b3c15d29e53
Gitthuma <george.githuma@gmail.com>
2021-11-01 19:46:24 +0300

    Create a replacement for a word in the Sentence

--- app.py
+++ app.py
@@ -2,3 +2,10 @@
 
 sentence = input("Enter your sentence: ")
 print(sentence)
+
+# Create a replacement for a word in the Sentence
+
+word1 = input("Enter the word you wish to replace: ")
+word2 = input("Enter the word you wish to replace it with: ")
+
+print(sentence.replace(word1, word2))
